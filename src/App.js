eb6d47b83821fb5ced2fe979bfa3999f2bb76379
stayfoolish24 <stayfoolish24@gmail.com>
2021-06-27 20:32:49 +0900

    customer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id' : 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '961111',
+  'gender' : '남자',
+  'job': '대학생'
+},
+{
+  'id' : 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name' : '유관순',
+  'birthday' : '921209',
+  'gender' : '여자',
+  'job': '프리'
+},
+{
+  'id' : 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name' : '김서방',
+  'birthday' : '551209',
+  'gender' : '남자',
+  'job': '회사원'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              id={c.id}
+              image={c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+          />
+          );
+        })
+      }
     </div>
   );
 }
